95e97997e691491652354235c696f8c537d0a96a
resquator <daryl.j.felix@gmail.com>
2021-12-10 13:08:49 +0100

    variable

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import logging
 
 app = Flask(__name__)
 
@@ -15,7 +16,21 @@ def results():
     if request.method == 'POST':
         model_file_path = 'rent_price_predictor.pkl'        
         model = pickle.load(open(model_file_path, 'rb'))
-        row_one = np.array([131,   2,   4,   0,   1]).reshape(1,-1) 
+        
+        surface = float(request.form['surface'])
+        level = float(request.form['level'])
+        room = float(request.form['room'])
+        garden = float(request.form['garden'])
+        garage = float(request.form['garage']) 
+        
+        print(" ")
+        print("----------------------------------------------------------")
+        print(f"Surface = {surface} of type {type(surface)}")
+        row_one = np.array([surface,   level,   room,   garden,   garage]).reshape(1,-1) 
+        #row_one.astype(np.float64)  
+        
+        logging.info(f"call the model with surface {surface}, level {level} ")
+
         predicted_rent_price = model.predict(row_one)
         return render_template('resultsform.html', prediction=np.round(predicted_rent_price[0],2))
 
